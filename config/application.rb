178a6af041ab9a2fc61dcc01ba65ae22622136d8
skyv26 <dev.aakashv@gmail.com>
2023-01-25 20:59:28 +0530

    Updated the `cors` setting with `wild card`

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module HelloRailsBackEnd
     config.load_defaults 7.0
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', 'https://hello-react-front-end.vercel.app/', 'https://hello-react-front-6ihsdbzhl-skyv26.vercel.app/'
+        origins '*', 'localhost:3000', 'https://hello-react-front-end.vercel.app/', 'https://hello-react-front-6ihsdbzhl-skyv26.vercel.app/'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end

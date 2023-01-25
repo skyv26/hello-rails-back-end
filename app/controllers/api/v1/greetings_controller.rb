8b7e53f09d79420ae721cd02264fb30cb4be3e27
skyv26 <dev.aakashv@gmail.com>
2023-01-25 18:22:25 +0530

    Add the `Greeting` Controller

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,10 @@
+module API
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        greetings = Message.all.sample.slice(:greeting)
+        render json: greetings
+      end
+    end
+  end
+end
